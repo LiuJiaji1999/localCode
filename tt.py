5612fcd349912938c2d4561b821af3bd0e6dbe0c
Liujiaji1999 <494882733@qq.com>
2024-07-13 19:41:23 +0800

    add

--- tt.py
+++ tt.py
@@ -1,8 +1,11 @@
 import codecs
 import os
-
+import torch
 # with open('example.txt', 'a') as file:
 #     file.write('Hello, Python!')
 
-with open('example.txt', 'a+') as file:
-    file.write('\nAppend this line.')
+# with open('example.txt', 'a+') as file:
+#     file.write('\nAppend this line.')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = torch.hub.load('ultralytics/ultralytics', 'yolov8m' ,pretrained=True).to(device)
