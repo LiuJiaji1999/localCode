c02a469eacd664e3b50a5ef1df199755c8502fc6
Liujiaji1999 <494882733@qq.com>
2025-07-10 14:14:19 +0800

    7zfile

---
+++ multimodal/7zfile.py
@@ -0,0 +1,5 @@
+
+import py7zr
+with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/MutilModel-494882733.7z", mode='r', password='XK3KUAtEFLnXPhW8915PROP353b2b4ee') as z:
+    z.extractall("/Users/rl/Documents/PhD-student/Untitled_Folder/")
+    print('解压成功')
