2e1795c29868d77416dc384c095571ea568315df
Liujiaji1999 <494882733@qq.com>
2025-07-11 18:35:36 +0800

    7zfile

--- multimodal/7zfile.py
+++ multimodal/7zfile.py
@@ -1,4 +1,4 @@
-
+# conda activate base
 import py7zr
 with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/MutilModel-494882733.7z", mode='r', password='XK3KUAtEFLnXPhW8915PROP353b2b4ee') as z:
     z.extractall("/Users/rl/Documents/PhD-student/Untitled_Folder/")
