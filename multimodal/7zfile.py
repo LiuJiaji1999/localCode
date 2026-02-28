e64fa5fefc7a611982f632d88b14d003c4c3cce6
Liujiaji1999 <494882733@qq.com>
2026-02-28 21:03:24 +0800

    add

--- multimodal/7zfile.py
+++ multimodal/7zfile.py
@@ -11,20 +11,20 @@
 #     print('MutilModel 解压成功')
 
 
-# # Ultralytics-YOLO
+# Ultralytics-YOLO
+import py7zr
+with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/Ultralytics-YOLO-494882733.7z", mode='r', password='JPDUw8Y8d5vPYvjYj1Pl4Oxe5a85c4d9') as z:
+    z.extractall("/Users/rl/Documents/PhD-student/Untitled_Folder/")
+    print('Ultralytics-YOLO 解压成功')
+
+
+# # DEIM
 # import py7zr
-# with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/Ultralytics-YOLO-494882733.7z", mode='r', password='JPDUw8Y8d5vPYvjYj1Pl4Oxe5a85c4d9') as z:
+# with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/DEIM-494882733.7z", mode='r', password='vz3wyu7CTV3r6YvySgfwoiQRd867372e') as z:
 #     z.extractall("/Users/rl/Documents/PhD-student/Untitled_Folder/")
-#     print('Ultralytics-YOLO 解压成功')
-
-
-# DEIM
-import py7zr
-with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/DEIM-494882733.7z", mode='r', password='vz3wyu7CTV3r6YvySgfwoiQRd867372e') as z:
-    z.extractall("/Users/rl/Documents/PhD-student/Untitled_Folder/")
-    print('DEIM 解压成功')
-# pycocoeval
-import py7zr
-with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/pycocoeval-494882733.7z", mode='r', password='vz3wyu7CTV3r6YvySgfwoiQRd867372e') as z:
-    z.extractall("/Users/rl/Documents/PhD-student/Untitled_Folder/")
-    print('pycocoeval 解压成功')
+#     print('DEIM 解压成功')
+# # pycocoeval
+# import py7zr
+# with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/pycocoeval-494882733.7z", mode='r', password='vz3wyu7CTV3r6YvySgfwoiQRd867372e') as z:
+#     z.extractall("/Users/rl/Documents/PhD-student/Untitled_Folder/")
+#     print('pycocoeval 解压成功')
