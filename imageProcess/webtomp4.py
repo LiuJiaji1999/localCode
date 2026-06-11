72bced5395143c75110163b2d1044a1c9bfbb620
Liujiaji1999 <494882733@qq.com>
2026-06-11 16:50:25 +0800

    add

---
+++ imageProcess/webtomp4.py
@@ -0,0 +1,25 @@
+import subprocess
+from pathlib import Path
+
+def webm_to_mp4(input_path, output_path=None):
+    input_path = Path(input_path)
+
+    if output_path is None:
+        output_path = input_path.with_suffix(".mp4")
+    else:
+        output_path = Path(output_path)
+
+    cmd = [
+        "ffmpeg",
+        "-i", str(input_path),
+        "-c:v", "libx264",
+        "-c:a", "aac",
+        "-strict", "experimental",
+        str(output_path)
+    ]
+
+    subprocess.run(cmd, check=True)
+    print(f"转换完成：{output_path}")
+
+if __name__ == "__main__":
+    webm_to_mp4("录屏 2026-06-11 15-54-03.webm")
--- multimodal/7zfile.py
+++ multimodal/7zfile.py
@@ -2,14 +2,14 @@
 # cd knowledge/multimodal/
 # python 7zfile.py
 
-# # MutilModel
+# MutilModel
 # import py7zr
 # with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/MutilModel-494882733.7z", mode='r', password='XK3KUAtEFLnXPhW8915PROP353b2b4ee') as z:
 #     z.extractall("/Users/rl/Documents/PhD-student/Untitled_Folder/")
 #     print('MutilModel 解压成功')
 
 
-# Ultralytics-YOLO
+# # Ultralytics-YOLO
 import py7zr
 with py7zr.SevenZipFile("/Users/rl/Documents/PhD-student/Untitled_Folder/Ultralytics-YOLO-494882733.7z", mode='r', password='JPDUw8Y8d5vPYvjYj1Pl4Oxe5a85c4d9') as z:
     z.extractall("/Users/rl/Documents/PhD-student/Untitled_Folder/")
